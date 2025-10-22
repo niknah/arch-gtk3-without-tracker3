1563326501ddac55e50403c4d517b5a31fd5285c
niknah <git@mail.niknah.com>
2025-10-22 16:46:08 +1100

    Change AppImage binary name

--- manager.sh
+++ manager.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-LD_LIBRARY_PATH=${SCRIPTPATH} ${SCRIPTPATH}/Manager-linux-x64.AppImage
+LD_LIBRARY_PATH=${SCRIPTPATH} ${SCRIPTPATH}/Manager-x86_64.AppImage
 
